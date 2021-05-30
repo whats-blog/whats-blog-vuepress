c59a234916e2632443c59a7a67e1a17df83ee679
XinPing Wang <xinping.wang@hotmail.com>
2021-05-30 21:04:59 +0800

    feat: simple layout to show content

--- packages/vuepress-theme-next/lib/node/index.js
+++ packages/vuepress-theme-next/lib/node/index.js
@@ -1,13 +1,16 @@
 const path = require('path')
 
-const nextTheme = () => {
+const nextTheme = (options) => {
   return {
     name: '@whats-blog/vuepress-theme-next',
     layouts: {
       Layout: path.resolve(__dirname, '../client/layouts/Layout.vue'),
       404: path.resolve(__dirname, '../client/layouts/404.vue')
     },
-    plugins: [require.resolve('@whats-blog/vuepress-plugin-blog')]
+    plugins: [
+      require.resolve('@whats-blog/vuepress-plugin-blog'),
+      [require.resolve('@vuepress/plugin-theme-data'), { themeData: options }]
+    ]
   }
 }
 
