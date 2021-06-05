437ee6150237ddeb5a7090ea5ef7950ac5a15eed
XinPing Wang <xinping.wang@hotmail.com>
2021-06-05 12:46:57 +0800

    feat: blog contend and config from create-vuepress-site

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,61 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://vuepress.github.io/reference/config.html#title
+   */
+  title: 'VuePress Blog Example',
+  /**
+   * Ref：https://vuepress.github.io/reference/config.html#description
+   */
+  description: description,
+  /**
+   * Ref：https://vuepress.github.io/reference/config.html#theme
+   */
+  theme: '@whats-blog/vuepress-theme-next',
+  /**
+   * Ref：https://vuepress.github.io/reference/config.html#themeconfig
+   */
+  themeConfig: {
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#modifyblogpluginoptions
+     */
+    modifyBlogPluginOptions(blogPluginOptions) {
+      return blogPluginOptions
+    },
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#nav
+     */
+    nav: [
+      {
+        text: 'Blog',
+        link: '/'
+      },
+      {
+        text: 'Tags',
+        link: '/tag/'
+      }
+    ],
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#footer
+     */
+    footer: {
+      contact: [
+        {
+          type: 'github',
+          link: 'https://github.com/whats-blog'
+        }
+      ],
+      copyright: [
+        {
+          text: 'Privacy Policy',
+          link: 'https://policies.google.com/privacy?hl=en-US'
+        },
+        {
+          text: "MIT Licensed | Copyright © 2021-present What's Blog",
+          link: ''
+        }
+      ]
+    }
+  }
+}
