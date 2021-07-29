4dba0f42f3153f736443c6b0f254719f51669864
XinPing Wang <xinping.wang@hotmail.com>
2021-07-30 06:56:31 +0800

    feat: tailwind config

---
+++ packages/vuepress-theme-next/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/client/**/*.css', './src/client/**/*.vue'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
